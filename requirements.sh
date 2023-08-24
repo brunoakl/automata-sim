8df415942353eaa01c6801c5f754a862272546c9
Bruno Machado Ferreira <64101776+brunoakl@users.noreply.github.com>
2023-08-24 09:52:31 -0400

    Prepara o ambiente
    Cria o ambiente virtual Conda e baixa o graphviz

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+conda create -n LFA;
+conda activate LFA;
+conda install python=3.10.9;
+sudo apt-get install graphviz graphviz-dev;
+pip install graphviz==0.20.1
+
+echo Seu ambiente está pronto para uso! Para reinstalar, execute "source reinstall.sh".
